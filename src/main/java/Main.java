abebc049f5d8a934e100eeb0132375d28ae6387f
Ewelina <ewelinaformella@gmail.com>
2019-07-01 21:15:08 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+
+        Robot roman = new Robot("Roman");
+
+
+
+        roman.przedstawRobota();
+
+
+
+    }
+}
+
---
+++ src/main/java/RuchRobota.java
@@ -0,0 +1,19 @@
+public enum RuchRobota {
+    KROK_LEWA(4),
+    KROK_PRAWA(4),
+    RUCH_REKA_LEWA(3),
+    RUCH_REKA_PRAWA(3),
+    SKOK(8),
+    ;
+    protected int zuzycieBaterii;
+
+    RuchRobota(int zuzycieBaterii) {
+        this.zuzycieBaterii = zuzycieBaterii;
+    }
+
+    public int getZuzycieBaterii() {
+        return zuzycieBaterii;
+    }
+
+
+}
